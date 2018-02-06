7a01da898fa8d2b3266c06aef988ceebe77e51c5
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-06 10:15:17 -0300

    ejercicio 1 y 2
    06-02

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,14 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre;
+	//Esto es un comentario de una linea
+	/*Esto es un bloque
+	alert(nombre);
+	alert("nombre");
+	nombre="Franco";
+	alert(nombre);*/
+	nombre = prompt("Introduce tu nombre", "Escribe aqui!!");
+	alert("El nombre ingresado es: "+nombre);
 }
 
