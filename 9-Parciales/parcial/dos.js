1c59ca312e63f53ec71e739ecf4492dfd3794914
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-07 01:40:24 -0300

    dos, tres

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,11 @@
 function Mostrar()
 {
-  
+ 	var importe;
+ 	var importeFinal;
+
+ 	importe = prompt("Ingrese el precio de producto","$$$"); 
+
+ 	importeFinal = importe*1.21;
+
+ 	document.getElementById("importeFinal").value = parseInt(importeFinal);
 }
--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var cantidadDeHilo;
+
+	largo = parseInt(document.getElementById("alrgo").value);
+	ancho = parseInt(document.getElementById("ancho").value);
+
+	perimetro = largo+largo+ancho+ancho;
+	cantidadDeHilo = perimetro*3;
+
+	alert("La cantidad de hilo que se necesita son "+cantidadDeHilo+" metros");
+
 
 }
