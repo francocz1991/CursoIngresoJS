befdd61f0a4a0c6e2cb82f43e7fcab7c2dce9f27
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-13 16:28:49 -0300

    cuatro

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,28 @@
 function Mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
 
+	numeroUno = prompt("Ingrese un numero");
+	numeroDos = prompt("Ingrese un numero");
+
+	if (numeroUno==numeroDos)
+	{
+		resultado = numeroUno*numeroDos;
+		document.write(resultado);
+	}
+	else
+	{
+		if(numeroUno>numeroDos)
+		{
+			resultado = numeroUno-numeroDos;
+			document.write(resultado);
+		}
+		else
+		{
+			resultado = parseInt(numeroUno)+parseInt(numeroDos);
+			document.write(resultado);
+		}
+	}
 }
