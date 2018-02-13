30c6b0dd5c86c95b1e394ee722262bb9a004bf61
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-13 16:30:31 -0300

    1, 2, 3

--- 9-Parciales/recuperatorio2/2-entradaSalida.js
+++ 9-Parciales/recuperatorio2/2-entradaSalida.js
@@ -1,7 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 2-EntradaSalida");
+	var importe;
+	var importeConIVA;
 	
+	importe = prompt('Ingrese importe');
+	importeConIVA = importe*1.21;
+	document.getElementById('importe').value = importeConIVA;
 }
 
--- 9-Parciales/recuperatorio2/3-entradaSalida.js
+++ 9-Parciales/recuperatorio2/3-entradaSalida.js
@@ -1,7 +1,20 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 3-EntradaSalida");
-	
+	var ancho;
+	var largo;
+	var perimetro;
+	var cantidadDeHilo;
+
+	ancho = document.getElementById('ancho').value;
+	largo = document.getElementById('largo').value;
+
+	ancho = parseInt(ancho);
+	largo = parseInt(largo);
+
+	perimetro = ancho+ancho+largo+largo;
+	cantidadDeHilo = 6*perimetro;
+
+	alert("La cantidad de hilo que se necesita es: "+cantidadDeHilo+" metros");
 }
 
