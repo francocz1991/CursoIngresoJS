6cd73ef380536765ce3f63de96a51066699333d6
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-14 09:54:17 -0300

    8

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -4,14 +4,22 @@ function Mostrar()
 
 	var edad;
 	var estadoCivil;
-
+	
 	edad = document.getElementById('edad').value;
 	estadoCivil = document.getElementById('estadoCivil').value;
 
-	if (edad>18 && estadoCivil=="Soltero")
+	if (edad<18 && estadoCivil!="Soltero")
 	{
-		alert('Es soltero y no es menor');
+		console.log("NO HACER NADA");
 	}
+	else
+	{
+		if (edad>17 && estadoCivil=="Soltero")
+		{
+			alert('Es soltero y no es menor');
+		}
+	}
+	
 
 
 
