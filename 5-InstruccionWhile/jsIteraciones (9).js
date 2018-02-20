032fd763db35d522f7c843e8c5040699a7e36225
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-20 12:07:43 -0300

    9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,14 +3,41 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var maximo;
+	var minimo;
+	var numero;
+
 	var respuesta='si';
 
 	while(respuesta!='no')
 	{
-		
+		contador++;
+		numero = prompt("Ingrese numero");
+		numero = parseInt(numero);
+
+		if (contador==1)
+		{
+			minimo=numero;
+			maximo=numero;
+		}
+		else
+		{
+			if (numero>maximo)
+			{
+				maximo = numero;
+			}
+			if (numero<minimo)
+			{
+				minimo = numero;
+			}
+		}
+
+		respuesta = prompt("Ingrese 'no' para terminar");
 	
 	}
+
+	document.getElementById('maximo').value = maximo;
+	document.getElementById('minimo').value = minimo;
 
 
 
