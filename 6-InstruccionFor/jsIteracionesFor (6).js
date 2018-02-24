4bad77c7a3b89c71b5739dedbdb8e87675537b45
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-24 01:13:06 -0300

    6, 7

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,20 @@
+
+var numero;
+var contadorDePares = 0;
+
 function Mostrar()
 {
-
-
-
+	numero =  prompt("Ingrese numero");
+	for(contador = 1; contador<numero; contador++)
+	{
+		if (contador%2==0)
+		{
+			document.write(contador+"<br>");
+			contadorDePares++;
+		}
+		
+	}
+	document.write("cantidad de Pares: "+contadorDePares);
 
 }//FIN DE LA FUNCIÓN
+
--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,7 +1,19 @@
+var numero;
+var contadorDeDivisores = 0;
+
 function Mostrar()
 {
+	numero = prompt("Ingrese numero");
+	
+	for(contador = 1; contador<numero; contador++)
+	{
+		if (numero%contador==0)
+		{
+			document.write(contador+"<br>");
+			contadorDeDivisores++;
+		}
+	}
 
-
-
+	document.write("Cantidad de divisores del numero "+numero+" es: "+contadorDeDivisores);
 
 }//FIN DE LA FUNCIÓN
