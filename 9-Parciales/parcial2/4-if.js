2a14909c6961c332c5776cb59e46aba9d2e9eda4
francocz1991 <36193624+francocz1991@users.noreply.github.com>
2018-02-28 11:59:18 -0300

    clase 28/02

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,10 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*no usar while, no validar.
+Se ingresan 2 numeros, si son iguales se concatenan, si el mayor es el primero se multiplica de lo contrario se resta, 
+si la multiplicacion es par, agregar la palabra "es par"*/
 function Mostrar()
 {
-	var numeroUno;
+	/*var numeroUno;
 	var numeroDos;
 	var resultado;
 
@@ -28,6 +31,49 @@ function Mostrar()
 			resultado = numeroUno+numeroDos;
 			document.write("<h1>"+resultado+"</h1>");
 		}
-	}	
+	}*/	
+
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	var multiplicacion;
+
+	numeroUno = prompt("ingrese primer numero");
+	numeroDos = prompt("ingrese segundo numero");
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if (numeroUno==numeroDos)
+	{
+		resultado=numeroUno+""+numeroDos;
+	}
+	else
+	{
+		if (numeroUno>numeroDos)
+		{
+			multiplicacion=numeroUno*numeroDos;
+			resultado=multiplicacion;
+		}
+		else
+		{
+			if (numeroUno<numeroDos)
+			{
+				resultado=numeroUno-numeroDos;
+			}
+			else
+			{
+				if ((numeroUno*numeroDos)!=0 && numeroUno*numeroDos%2==0)
+				{
+					resultado = resultado+"es`par";
+				}
+			}
+		}
+	}
+
+	
+
+	
+
+	alert(resultado);
 }
 
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,122 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*se ingresa hasta que el usuario quiera: el nombre de un animal,
+el peso del mismo(validar que sea mayor a o) y la temperatura del habitat de este animal(validar entre -40 y 40),
+informar la cantidad de temperaturas pares, la cantidad de temperaturas impares, el nombre del animal mas pesado,
+el nombre del animal menos pesado, la cantidad de animales que viven en habitat menores a o inclusive,
+el promedio del peso de todos los animales, la temperatura maxima y minima.*/
 function Mostrar()
 {
-	alert("Funciona 8-iteraciones");
+	var salir="";
+	var animal;
+	var peso;
+	var temperatura;
+	var contadorTemperaturaPar = 0; 
+	var contadorTemperaturaImpar = 0; 
+	var contador = 0;
+	var pesoAnimalMasPesado = 0;
+	var pesoAnimalMenosPesado = 0;
+	var animalMasPesado;
+	var animalMenosPesado;
+	var animalHabitat = 0;
+	var temperaturaMaxima = 0;
+	var temperaturaMinima = 0;
+	var promedio;
+	var acumuladorPeso =0;
+
+	while(salir!="no")
+	{
+		animal=prompt("Ingrese animal");
+
+		peso=prompt("Ingrese peso del animal");
+		peso=parseInt(peso);
+		while(peso<0 || isNaN(peso))
+		{
+			peso=prompt("Reingrese peso del animal");
+			peso=parseInt(peso);
+		}
+
+		temperatura=prompt("Ingrese temperatura");
+		temperatura=parseInt(temperatura);
+		while(temperatura<-40 || temperatura>40 || isNaN(temperatura))
+		{
+			temperatura=prompt("Reingrese temperatura");
+			temperatura=parseInt(temperatura);
+		}
+
+		if (temperatura!=0 && temperatura%2==0)
+		{
+			contadorTemperaturaPar++;
+		}
+		else
+		{
+			if (temperatura!=0 && temperatura%2!=0)
+			{
+				contadorTemperaturaImpar++;	
+			}			
+		}
+
+		if (contador==0)
+		{
+			pesoAnimalMasPesado = peso;
+			pesoAnimalMenosPesado = peso;
+			animalMasPesado = animal; 
+			animalMenosPesado=animal;
+		}
+		{
+			if (peso>pesoAnimalMasPesado)
+			{
+				pesoAnimalMasPesado=peso;
+				animalMasPesado = animal;
+			}
+			else
+			{
+				pesoAnimalMenosPesado=peso;
+				animalMenosPesado=animal;
+			}
+		}
+
+		if (temperatura<0)
+		{
+			animalHabitat++;
+		}
+
+		if (contador==0)
+		{
+			temperaturaMaxima=temperatura;
+			temperaturaMinima=temperatura;
+		}
+		else
+		{
+			if (temperatura>temperaturaMaxima)
+			{
+				temperaturaMaxima=temperatura;
+			}
+			else
+			{
+				if (temperatura<temperaturaMinima)
+				{
+					temperaturaMinima=temperatura;
+				}
+			}
+		}
+
+		acumuladorPeso = acumuladorPeso+peso;
+		contador++;
+		salir=prompt("'no' para salir");
+	}
+
+	promedio=acumuladorPeso/contador;
+
+	document.write("Temperaturas pares: "+contadorTemperaturaPar+"<br>");
+	document.write("temperaturas impares: "+contadorTemperaturaImpar+"<br>");
+	document.write("Animal mas pesado: "+animalMasPesado+"<br>");
+	document.write("Animal menos pesado: "+animalMenosPesado+"<br>");
+	document.write("Animales que viven en habitat iguales o menores a CERO: "+animalHabitat+"<br>");
+	document.write("Promedio de pesos: "+promedio+"<br>");
+	document.write("Temperatura maxima: "+temperaturaMaxima+"<br>");
+	document.write("Temperatura minima: "+temperaturaMinima+"<br>");
+
+}	
+
 	
-}
 
